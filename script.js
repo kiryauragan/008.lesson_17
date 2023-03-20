224f9a8b36a6f7c35e144a49d932479da9fa5e5c
Kyrylo <kirilltaranskyi@gmail.com>
2023-03-20 10:01:17 +0200

    script.js updated

--- script.js
+++ script.js
@@ -3,19 +3,19 @@
 let array = [1, "coke", 47, 199, "apple", null];
 
 function average(array) {
-	let sum = 0;
-	let count = 0;
-	for (let i = 0; i < array.length; i++) {
-		if (typeof array[i] === "number") {
-			sum += array[i];
-			count++;
-		}
-	}
-	if (count === 0) {
-		return null;
-	} else {
-		return sum / count;
-	}
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      sum += array[i];
+      count++;
+    }
+  }
+  if (count === 0) {
+    return null;
+  } else {
+    return sum / count;
+  }
 }
 
 console.log(`1. Середне арифметичне: ${average(array)}`);
@@ -26,68 +26,72 @@ let x = Number(prompt("Введіть перше число:"));
 let y = Number(prompt("Введіть друге число:"));
 let znak = prompt("Введіть знак математичної операції (+, -, *, /, %, ^):");
 
-if (x === null || y === null || znak === null) {
-	alert("Спробуйте ще раз.");
-} else {
-	function doMath(x, znak, y) {
-		if (znak === "/" && y === 0) {
-			return "На нуль ділити неможливо!";
-		}
-
-		switch (znak) {
-			case "+":
-				return x + y;
-			case "-":
-				return x - y;
-			case "*":
-				return x * y;
-			case "/":
-				return x / y;
-			case "%":
-				return x % y;
-			case "^":
-				return Math.pow(x, y);
-			default:
-				return "Введіть знак математичної операції";
-		}
-	}
-	console.log(
-		`2. Результат математичної дії: ${x} ${znak} ${y} = ${doMath(x, znak, y)}`
-	);
+function doMath(x, znak, y) {
+  if (znak === "/" && y === 0) {
+    return "На нуль ділити неможливо!";
+  } else if (x === null || y === null || znak === null) {
+    alert("Спробуйте ще раз.");
+  } else {
+    let result = calculate(x, znak, y);
+    console.log(result);
+  }
 }
+
+function calculate(x, znak, y) {
+  switch (znak) {
+    case "+":
+      return x + y;
+    case "-":
+      return x - y;
+    case "*":
+      return x * y;
+    case "/":
+      return x / y;
+    case "%":
+      return x % y;
+    case "^":
+      return Math.pow(x, y);
+    default:
+      return "Введіть знак математичної операції";
+  }
+}
+
+console.log(
+  `2. Результат математичної дії: ${x} ${znak} ${y} = ${calculate(x, znak, y)}`
+);
 
 // 3. Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
 
 function addArray(rows, columns) {
-	let array = [];
-	for (let i = 0; i < rows; i++) {
-		let insideArray = [];
-		for (let j = 0; j < columns; j++) {
-			let value = prompt(`Введіть значення елементу масива [${i}][${j}]`);
-			insideArray.push(value);
-		}
-		array.push(insideArray);
-	}
-	return array;
+  let array = [];
+  for (let i = 0; i < rows; i++) {
+    let insideArray = [];
+    for (let j = 0; j < columns; j++) {
+      let value = prompt(`Введіть значення елементу масива [${i}][${j}]`);
+      insideArray.push(value);
+    }
+    array.push(insideArray);
+  }
+  return array;
 }
 
 console.log(addArray(2, 2));
 
 //4. Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 
-function removeChars(str, symbols) {
-	let symbolsArray = symbols.split("");
-	let result = "";
-	for (let i = 0; i < str.length; i++) {
-		let char = str[i];
-		if (!symbolsArray.includes(char)) {
-			result += char;
-		}
-	}
-	return result;
-}
-
 let str = prompt("Введіть рядок:", "hello world");
 let symbols = prompt("Введіть символи для видалення:", "l d");
+symbols.split();
 
-alert(removeChars(str, symbols));
+function removeChars(str, symbols) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (!symbols.includes(char)) {
+      result += char;
+    }
+  }
+  return result;
+}
+
+console.log(removeChars(str, symbols));
